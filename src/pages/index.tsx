52304172ebd5c4253bdd6d2feed23e14726b2615
Matheus Cruz <matheuswachcruz@gmail.com>
2022-05-02 11:43:24 -0300

    feat: configure initial projetc structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Feedback Widget</title>
+      </Head>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
