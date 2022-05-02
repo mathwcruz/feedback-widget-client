597e6a4ca8ee54d3ad1dd707acd38931004ce760
Matheus Cruz <matheuswachcruz@gmail.com>
2022-05-02 15:59:31 -0300

    feat: create initial Widget component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import { Widget } from "components/Widget";
+
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Feedback Widget</title>
       </Head>
-      <h1>Hello World</h1>
+      <Widget />
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#34CB79",
+          600: "#34CB67",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
