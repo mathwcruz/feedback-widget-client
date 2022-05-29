14b48e81b4567b380a5b4acf7b55a850910f6a54
Matheus Cruz <matheuswachcruz@gmail.com>
2022-05-29 16:56:12 -0300

    refactor: remove last tranlstion logic

--- src/components/Benefits/index.tsx
+++ src/components/Benefits/index.tsx
@@ -1,25 +1,20 @@
-import { useTranslation } from "hooks/useTranslation";
 import { benefits } from "utils/benefits";
 
 import { Benefit } from "./Benefit";
 
 export const Benefits = () => {
-  const { currentTranslation } = useTranslation();
-
   return (
     <div className="w-full flex flex-col items-center justify-center mt-12">
       <span className="block text-primary-100 text-center text-base leading-5 font-medium">
-        {currentTranslation?.components?.benefits?.default?.title ||
-          "360° CUSTOMIZABLE"}
+        360° CUSTOMIZABLE
       </span>
       <div className="flex flex-col items-center justify-center gap-5 mt-2">
         <h3 className="text-center max-w-[335px] text-[32px] leading-[42px] text-primary-100 font-medium">
-          {currentTranslation?.components?.benefits?.default?.heading ||
-            "Have full control over your online store"}
+          Have full control over your online store
         </h3>
         <p className="text-center max-w-[335px] text-black-400 font-normal text-sm leading-6">
-          {currentTranslation?.components?.benefits?.default?.paragraph ||
-            "Take total control of your eCommerce business by owning the source code and data that drive it."}
+          Take total control of your eCommerce business by owning the source
+          code and data that drive it.
         </p>
       </div>
       <ul className="mt-9 flex flex-col items-center justify-center gap-6">
